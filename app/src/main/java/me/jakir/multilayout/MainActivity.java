d98bb0e07c5bbaa3bcfbfaa046eb17e0d5164145
Jakir Hossain <jakirseu@gmail.com>
2019-12-01 12:55:50 +0400

    update project

--- app/src/main/java/me/jakir/multilayout/MainActivity.java
+++ app/src/main/java/me/jakir/multilayout/MainActivity.java
@@ -1,14 +1,14 @@
 package me.jakir.multilayout;
 
-import android.support.v7.app.ActionBarActivity;
+
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends AppCompatActivity {
     Button button1;
     Button button2;
     Button button3;
@@ -43,26 +43,4 @@ public class MainActivity extends ActionBarActivity {
         });
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
